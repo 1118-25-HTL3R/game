3e92a591c46cb2b707d21eb5161fda2a2c0ef75c
1118-25-HTL3R <1118@htl.rennweg.at>
2024-12-31 22:04:58 +0100

    second commit

---
+++ src/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <md-app
+    v-cloak
+    md-waterfall
+    md-mode="fixed"
+  >
+    <md-app-toolbar class="md-primary md-dense">
+      <div class="md-toolbar-section-start">
+        <router-link to="/" class="md-title">Azure Cloud Integration - Game</router-link>
+      </div>
+    </md-app-toolbar>
+
+    <md-app-content>
+      <game/>
+    </md-app-content>
+  </md-app>
+</template>
+
+<script>
+import Game from "@/components/Game";
+export default {
+
+    name: 'App',
+
+    components: {
+      Game
+    },
+
+    methods: {
+        answer() {
+            return 42
+        },
+    },
+}
+</script>
+
+<style scoped>
+</style>
